ac1125a397f4437df1ce7cc257107058c95b0af8
9mithun9 <9mithun9@gmail.com>
2025-05-14 12:44:41 +0000

    V3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle 
 
 tokenizer = pickle.load(open("models/cv.pkl", "rb"))
@@ -17,6 +17,20 @@ def predcit():
     prediction = model.predict(tokenized_email)
     prediction = 1 if prediction == 1 else -1
     return render_template('index.html', text=email_text, prediction = prediction )
+
+
+
+@app.route('/api/predict', methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)  # Get data posted as a json
+    email_text = data['email-content']
+    tokenized_email = tokenizer.transform([email_text]) # X 
+    prediction = model.predict(tokenized_email)
+    # If the email is spam prediction should be 1
+    prediction = 1 if prediction == 1 else -1
+    return jsonify({prediction: prediction})
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
